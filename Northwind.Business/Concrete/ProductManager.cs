6923b22100b355c655b6d7dd1384e9e847006b52
EnginKisin <101111960+EnginKisin@users.noreply.github.com>
2022-03-29 21:52:54 +0300

    Add files via upload

---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,76 @@
+﻿
+using FluentValidation;
+using Northwind.Business.Abstract;
+using Northwind.Business.ValidationRules.FluentValidation;
+using Northwind.DataAccess.Abstract;
+using Northwind.DataAccess.Concrete;
+using Northwind.DataAccess.Concrete.EntityFramework;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+//kurumsal projelerde, Business kısmına entityframework paketi eklememeliydik.entityframework e baglı kaldık.NHibarnate i etkiledik.
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //Business code
+
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetProductsByCategory(int categoryId)
+        {
+            return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+
+        public List<Product> GetProductsByProduct(string productName)
+        {
+            return _productDal.GetAll(p => p.ProductName.ToLower().Contains(productName.ToLower()));
+        }
+
+        public void Add(Product product)
+        {
+            ProductValidator productValidator = new ProductValidator();
+            var result = productValidator.Validate(product);
+            if (result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+            _productDal.Add(product);
+        }
+
+
+        public void Update(Product product)
+        {
+            _productDal.Update(product);
+        }
+
+        public void Delete(Product product)
+        {
+            try
+            {
+                _productDal.Delete(product);
+            }
+            catch
+            {
+                throw new Exception("Silme Gerçekleşemedi!");
+            }
+        }
+
+
+    }
+}
